004cd078c16dff5680ea8e5282d40a24dfb09516
jasonw52603 <minecrafter10k@gmail.com>
2019-11-28 17:38:22 -0500

    added RobotMap, TankDrive, CTRE Libs

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,28 @@
+package frc.robot;
+
+public class RobotMap {
+
+	// Ports not on robot
+	// Joysticks
+	public static final int JOYSTICK_DRIVE_LEFT = 0;
+	public static final int JOYSTICK_DRIVE_RIGHT = 1;
+	public static final int JOYSTICK_MANIPULATOR = 2;
+
+	//Joystick Buttons
+
+	// Robot Ports
+	// Analog Ports
+
+	// Drive Train Motors
+	public static final int MOTOR_FRONT_LEFT = 0;
+	public static final int MOTOR_FRONT_RIGHT = 1;
+	public static final int MOTOR_BACK_LEFT = 2;
+	public static final int MOTOR_BACK_RIGHT = 3;
+
+	// Drive Train Encoders
+	public static final int DRIVE_TRAIN_LEFT_ENCODER_A = 0;
+	public static final int DRIVE_TRAIN_LEFT_ENCODER_B = 1;
+	public static final int DRIVE_TRAIN_RIGHT_ENCODER_A = 2;
+	public static final int DRIVE_TRAIN_RIGHT_ENCODER_B = 3;
+
+}
