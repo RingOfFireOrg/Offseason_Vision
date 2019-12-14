4bac7eccd15d061aa27356e4f726fc54d97529e8
jasonw52603 <minecrafter10k@gmail.com>
2019-12-14 13:42:00 -0500

    added encoder class

---
+++ src/main/java/frc/robot/Encoders.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Encoder;;
+
+public class Encoders {
+    private Encoder leftEncoder = new Encoder(RobotMap.DRIVE_TRAIN_LEFT_ENCODER_A,
+                                              RobotMap.DRIVE_TRAIN_LEFT_ENCODER_B, false, Encoder.EncodingType.k1X);
+    private Encoder rightEncoder = new Encoder(RobotMap.DRIVE_TRAIN_RIGHT_ENCODER_A, 
+                                               RobotMap.DRIVE_TRAIN_RIGHT_ENCODER_B, false, Encoder.EncodingType.k1X);
+}
