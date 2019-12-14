739fd309a9dc86edcfab2dab894a6d245e97aa3f
jasonw52603 <minecrafter10k@gmail.com>
2019-12-14 13:32:10 -0500

    added drive method to NeoTankDrive class

--- src/main/java/frc/robot/NeoTankDrive.java
+++ src/main/java/frc/robot/NeoTankDrive.java
@@ -6,10 +6,16 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 
 public class NeoTankDrive {
-    {new SpeedControllerGroup(
+    SpeedControllerGroup rightMotors = new SpeedControllerGroup(
         new CANSparkMax(RobotMap.MOTOR_FRONT_RIGHT, MotorType.kBrushless),
         new CANSparkMax(RobotMap.MOTOR_BACK_RIGHT, MotorType.kBrushless));
-    new SpeedControllerGroup(
+    SpeedControllerGroup leftMotors = new SpeedControllerGroup(
         new CANSparkMax(RobotMap.MOTOR_FRONT_LEFT, MotorType.kBrushless),
-        new CANSparkMax(RobotMap.MOTOR_BACK_LEFT, MotorType.kBrushless));}
+        new CANSparkMax(RobotMap.MOTOR_BACK_LEFT, MotorType.kBrushless));
+
+    public void drive(double rightSpeed, double leftSpeed){
+        rightMotors.set(rightSpeed);
+        leftMotors.set(leftSpeed);
+    }
 }
+
